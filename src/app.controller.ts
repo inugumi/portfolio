baf62839fc03734e013e987b05c42fa8189082c4
cbacken <cbacken@gmail.com>
2020-09-03 17:56:39 -0700

    Initial commit

---
+++ src/app.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get('health')
+  getHealth(): object {
+    return this.appService.getStatus();
+  }  
+
+  @Get('status')
+  getStatus(): object {
+    return this.appService.getStatus();
+  }
+}
